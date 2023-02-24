46747698f1ab27f7831280a94ab189944344ca57
Software <96375724+Asilbekrasul@users.noreply.github.com>
2023-02-24 13:16:46 +0300

    updated

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const body = document.body
+const night = document.querySelector("#night")
+const sun = document.querySelector("#sun")
+
+sun.classList.toggle("hidden")
 
 document.querySelector("#btn").addEventListener("click" , ()=> {
     document.querySelector("body").classList.toggle("dark")
+    sun.classList.toggle("hidden");
+    night.classList.toggle("hidden")
 })
